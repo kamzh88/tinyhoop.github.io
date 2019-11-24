8575d05bde893c09eb4b8f37a632bd15ede769aa
KinKam Zhao <Kamzh88@KinKams-MacBook-Air.local>
2019-11-24 13:48:01 -0500

    readme pictures

---
+++ listofitemsforsale.js
@@ -0,0 +1,80 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_DB"
+});
+
+connection.connect(function (err, res) {
+    if (err) throw err;
+    list();
+})
+
+function list() {
+    var query = "SELECT * FROM products"
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.log(`\n Items available for sale \n`)
+        for (var i = 0; i < res.length; i++) {
+            console.log(
+                ` ID: ${res[i].id} \n Product: ${res[i].product_name} \n Department: ${res[i].department_name} \n Price: $${res[i].price} \n Quantity: ${res[i].stock_quantity} \n`);
+        }
+        buy(res);
+    })
+}
+
+function buy(res) {
+    inquirer
+        .prompt([
+            {
+                name: "buy",
+                type: "input",
+                message: "What item would you like to purchase? (Please input the ID)"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many items would you like to purchase?"
+            }
+        ])
+        
+        .then(function (answer, res) {
+            console.log(res);
+            var buyID = answer.buy - 1;
+            // var buyItem = res[buyID].stock_quantity;
+            var stock = answer.quantity
+            var quantityLeft = buyItem - stock;
+            console.log("stock quantity: " + buyItem);
+            console.log("how much I'll like to buy: " + stock)
+            switch (true) {
+
+                case (buyItem < stock):
+                    console.log("Not enough items in stock");
+                    break;
+                
+                case (buyItem >= stock):
+                    fulfill(quantityLeft, buyID, res);
+                    cost(stock, res, buyID);
+            }
+        })
+}
+
+function fulfill(quantityLeft, buyID, res) {
+    var product_name= `${res[buyID].product_name}`
+    var query = `UPDATE products SET stock_quantity= ${quantityLeft} WHERE id= ${res[buyID].id}`;
+    // console.log(quantityLeft);
+    connection.query(query, function (err) {
+        if (err) throw err;
+        console.log(`${product_name} has ${quantityLeft} left`);
+    })
+}
+
+function cost(stock, res, buyID){
+    var price = res[buyID].price
+    var costOfProduct = stock*price
+    console.log('Total cost of purchase $' + costOfProduct);
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+<!-- [text describing the image](Images/00-Server.png) -->
+
+![customer_view_item_for_sale](images/customer_view_item_for_sale.png)
+
+![customer_view_buy_product](images/customer_view_buy_product.png)
+
+![customer_view_item_out_of_stock](images/customer_view_item_out_of_stock.png)
+
+![manager_view_item_view](images/manager_item_view.png)
+
+![manager_view_Low_inventory](images/manager_view_Low_inventory.png)
+
+![manager_view_add_inventory](images/manager_view_add_inventory.png)
+
+![manager_view_product_added](images/manager_view_product_added.png)
+
+![manager_view_product_added_picture](images/manager_view_product_added_picture.png)
